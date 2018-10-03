b7be043ae967b89a1ba088b692638a22a017d35a
Petr Jurak <pjurak@redhat.com>
2018-10-03 13:45:58 +0200

    RESTEASY-1221 reproducer

---
+++ RESTEASY-1221/src/main/java/org/jboss/as/quickstarts/rshelloworld/Application.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.rshelloworld;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * @author pjurak
+ *
+ */
+//@ApplicationPath("/rest")
+public class Application extends javax.ws.rs.core.Application
+{
+
+}
